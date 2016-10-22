7900671a0a284d6c6b092345b984f6b4e165e427
Seth Koch <sethkoch1@gmail.com>
2016-10-22 17:15:31 -0500

    Connected Mongo database to project

--- server/app.js
+++ server/app.js
@@ -3,10 +3,20 @@
 var express = require('express');
 var app = express();
 
+var mongoUtil = require('./mongoUtil');
+mongoUtil.connect();
+
 app.use(express.static(__dirname + '/../client'));
 
 app.get('/employers', function(request, response){
-  response.json(["Hardees", "Walmart"]);
-})
+  let employment = mongoUtil.employers();
+  employment.find().toArray((err,docs) => {
+    if(err) {
+      response.sendStatus(400);
+    }
+    let employmentNames = docs.map((item) => item.name);
+    response.json(employmentNames);
+  });
+});
 
 app.listen(process.env.port || 8181);
